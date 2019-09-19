92f7d85f2ca79e3c765b355a21b8b84eadd3a8ea
gre7g <hafeliel@yahoo.com>
2019-09-18 21:45:29 -0500

    Scrollbars and data fetching

--- style_stripper/model/template_panel.py
+++ style_stripper/model/template_panel.py
@@ -38,17 +38,37 @@ class TemplatePanel(wx.Panel):
         add_stretcher(sizer2)
         text = wx.StaticText(self, label=_("Variant:"))
         sizer2.Add(text, 0, wx.CENTER, 0)
-        self.variant = wx.ScrollBar(self)
+        panel = wx.Panel(self, style=wx.BORDER_THEME)
+        sizer2b = wx.BoxSizer(wx.VERTICAL)
+        panel.SetSizer(sizer2b)
+        self.variant = wx.ScrollBar(panel, style=wx.SB_HORIZONTAL)
         self.variant.SetScrollbar(0, 1, 10, 1)
         self.variant.Bind(wx.EVT_SCROLL, self.app.frame_controls.on_variant)
-        sizer2.Add(self.variant, 1, wx.LEFT | wx.CENTER, 10)
+        sizer2b.Add(self.variant, 1, wx.EXPAND)
+        sizer2.Add(panel, 1, wx.LEFT | wx.CENTER, 10)
         self.item = wx.StaticText(self, label=_("1 of 10"))
         sizer2.Add(self.item, 0, wx.LEFT | wx.CENTER, 5)
 
         add_stretcher(sizer1)
 
+        panel = wx.Panel(self, style=wx.BORDER_THEME)
+        sizer2a = wx.BoxSizer(wx.VERTICAL)
+        panel.SetSizer(sizer2a)
+        page = wx.ScrollBar(panel, style=wx.SB_HORIZONTAL)
+        page.SetScrollbar(0, 1, 5, 1)
+        sizer2a.Add(page, 1, wx.EXPAND)
+        sizer1.Add(panel, 0, wx.EXPAND, 0)
+
         sizer3 = wx.BoxSizer(wx.HORIZONTAL)
-        sizer1.Add(sizer3, 0, wx.EXPAND, 0)
+        sizer1.Add(sizer3, 0, wx.EXPAND | wx.TOP, 10)
+        button = wx.Button(self, label=_("Prev"))
+        button.Bind(wx.EVT_BUTTON, self.app.frame_controls.on_prev)
+        sizer3.Add(button, 0, 0, 0)
+        add_stretcher(sizer3)
+        text = wx.StaticText(self, label=_("Estimated pages with this template:"))
+        sizer3.Add(text, 0, wx.CENTER, 0)
+        text = wx.StaticText(self, label=_("395"))
+        sizer3.Add(text, 0, wx.CENTER | wx.LEFT, 5)
         add_stretcher(sizer3)
         button = wx.Button(self, label=_("Next"))
         button.Bind(wx.EVT_BUTTON, self.app.frame_controls.on_next)
@@ -78,13 +98,40 @@ if __name__ == "__main__":
         "header_distance": section.header_distance / CONSTANTS.MEASURING.EMUS_PER_INCH,
         "footer_distance": section.footer_distance / CONSTANTS.MEASURING.EMUS_PER_INCH,
         "gutter": section.gutter / CONSTANTS.MEASURING.EMUS_PER_INCH,
-        "font": normal.font.name,
-        "font_size": normal.font.size / CONSTANTS.MEASURING.EMUS_PER_POINT,
-        "italic": bool(normal.font.italic),
-        "bold": bool(normal.font.bold),
-        "line_spacing": normal.paragraph_format.line_spacing / CONSTANTS.MEASURING.EMUS_PER_INCH,
-        "first_line_indent": normal.paragraph_format.first_line_indent / CONSTANTS.MEASURING.EMUS_PER_INCH
+        "first_line_indent": normal.paragraph_format.first_line_indent / CONSTANTS.MEASURING.EMUS_PER_INCH,
+        "line_spacing": normal.paragraph_format.line_spacing / CONSTANTS.MEASURING.EMUS_PER_INCH
     }
+    for name in ["Normal", "Heading 1", "Heading 2", "Header", "Footer", "Separator", "First Paragraph"]:
+        details[name] = {}
+        for attr, func in [
+            ("font", lambda s: s.font.name),
+            ("font_size", lambda s: s.font.size),
+            ("italic", lambda s: s.font.italic),
+            ("bold", lambda s: s.font.bold),
+            ("alignment", lambda s: s.paragraph_format.alignment),
+            ("space_before", lambda s: s.paragraph_format.space_before),
+            ("space_after", lambda s: s.paragraph_format.space_after)
+        ]:
+            s = style[name]
+            while True:
+                value = func(s)
+                details[name][attr] = value
+                if value is None:
+                    if s.base_style is None:
+                        break
+                    else:
+                        s = style[s.base_style.name]
+                else:
+                    break
+        # details[name] = {
+        #     "font": s.font.name,
+        #     "base": None if s.base_style is None else s.base_style.name,
+        #     "font_size": (s.font.size or 0) / CONSTANTS.MEASURING.EMUS_PER_POINT,
+        #     "space_before": (s.paragraph_format.space_before or 0) / CONSTANTS.MEASURING.EMUS_PER_POINT,
+        #     "space_after": (s.paragraph_format.space_after or 0) / CONSTANTS.MEASURING.EMUS_PER_POINT,
+        #     "italic": bool(s.font.italic),
+        #     "bold": bool(s.font.bold),
+        # }
     from pprint import pprint
     pprint(details)
 
