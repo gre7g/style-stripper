207667c4f2658a6a4550a865e939fe4c0313227a
gre7g <hafeliel@yahoo.com>
2019-09-28 17:02:36 -0500

    Not sure why this changed

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-pyinstaller "style_stripper\StyleStripper.py" ^
+pyinstaller "style_stripper\StyleStripper.pyw" ^
     --add-data="README.md;." ^
     --add-data="style_stripper\data\lorem_ipsum.txt;data" ^
     --add-data="style_stripper\docx_templates\*.docx;docx_templates"
